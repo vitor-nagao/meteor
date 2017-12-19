7c5519bf232574b41904ce5ec4841b7daccd2f2a
Ben Newman <ben@meteor.com>
2017-12-19 15:42:31 -0500

    Fix tests for now-deprecated stylus package.
    The last version of stylus published did not contain a version of the
    caching-compiler package that these tests now depend on, so I've removed
    the extra architecture matching for the stylus package (not for less).

--- tools/tests/compiler-plugins.js
+++ tools/tests/compiler-plugins.js
@@ -143,7 +143,8 @@ selftest.define("compiler plugin caching - coffee", () => {
       cacheMatch(
         "Ran (#" + n + ") on: " +
           JSON.stringify(files) +
-          (arch ? " " + JSON.stringify([arch]) : "")
+          ((arch && packageName !== "stylus")
+           ? " " + JSON.stringify([arch]) : "")
       );
     }
 
